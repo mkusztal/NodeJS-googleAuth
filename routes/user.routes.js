e2bb153ee6061450863a19d03bbce20077b8ea7c
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-22 11:28:16 +0200

    Add new subpages profile and profile/settings

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,22 @@ router.get('/logged', (req, res) => {
   res.render('logged');
 });
 
+router.get('/profile', (req, res) => {
+  if (req.user) {
+    res.render('profile');
+  } else {
+    res.render('noPermission');
+  }
+});
+
+router.get('/profile/settings', (req, res) => {
+  if (req.user) {
+    res.render('settings');
+  } else {
+    res.render('noPermission');
+  }
+});
+
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
